954c62724743fba4f9c381a8c6b9d2adf7c9bc7d
Levi Lais <levilais@gmail.com>
2017-09-22 09:46:47 -0700

    test commit 2

--- Random Ruby/HomeViewController.swift
+++ Random Ruby/HomeViewController.swift
@@ -15,7 +15,7 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        Utilities().setButtonShadow(button: playButton)
+        Utilities().setButtonShadow(button: playButton)
     }
     
     // ACTIONS
