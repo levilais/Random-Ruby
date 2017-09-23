11171c4f276993e00838fb21d705d6a4560fe652
Levi Lais <levilais@gmail.com>
2017-09-22 19:30:45 -0700

    Switched to "Safe Layout Guides"

---
+++ Random Ruby/CoreDataHelper.swift
@@ -0,0 +1,12 @@
+//
+//  CoreDataHelper.swift
+//  Random Ruby
+//
+//  Created by Levi on 9/22/17.
+//  Copyright © 2017 App Volks. All rights reserved.
+//
+
+// NOTICE: When ready to add CloudKit, go here for direction: https://stackoverflow.com/questions/40034803/sync-an-ios-app-that-uses-core-data-to-cloud
+// Another one that might help: https://medium.com/@guilhermerambo/synchronizing-data-with-cloudkit-94c6246a3fda
+
+import Foundation
